a9fe499a45e41c567f582ccf1f53d824b32be15d
Piotr Jurek <piotrjurek9807@gmail.com>
2022-04-11 23:14:30 +0200

    WebApp initialization

---
+++ JohannesWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JohannesWebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
